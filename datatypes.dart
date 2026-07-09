d07eb24f8a13161816edeff425e8e51e824aeb0e
V LAKSHMI PRASANNA <24wh1a05br@bvrithyderabad.edu.in>
2026-07-09 11:51:25 +0530

    Add basic data types demonstration in Dart

---
+++ datatypes.dart
@@ -0,0 +1,27 @@
+void main() {
+  
+  int age = 20;
+  print("int: $age");
+
+  double height = 5.8;
+  print("float(double): $height");
+
+  bool isStudent = true;
+  print("bool: $isStudent");
+
+  String name = "Lakshmi";
+  print("String: $name");
+
+  List<String> fruits = ["Apple", "Banana", "Mango"];
+  print("List: $fruits");
+
+  Map<String, dynamic> student = {
+    "Name": "Lakshmi",
+    "Age": 20
+  };
+  print("Map: $student");
+
+  Set<int> numbers = {10, 20, 30};
+  print("Set: $numbers");
+
+}
